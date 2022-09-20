4b41b847c6fdaddd10daa47757d25dc903d8a38d
TayLee <89365465+taehyuklee@users.noreply.github.com>
2022-09-21 08:19:52 +0900

    Update SamsungTennagerShark.java

--- Algorithm-Samsung/JavaVersion/SamsungTennagerShark.java
+++ Algorithm-Samsung/JavaVersion/SamsungTennagerShark.java
@@ -43,7 +43,7 @@ class Shark implements Cloneable{
 
 public class TennagerShark{
 	
-	static List<Fish> fishList = new ArrayList<>();
+//	static List<Fish> fishList = new ArrayList<>();
 	
 	//direction
 	static int[] dx = {-1,-1,0,1,1,1,0,-1};
@@ -51,7 +51,7 @@ public class TennagerShark{
 	
 	static int[][] fishes = new int[4][4];
 	
-	public static Fish findByCoord(int x, int y) {
+	public static Fish findByCoord(int x, int y, List<Fish> fishList) {
 		for(int i=0; i<fishList.size(); i++) {
 			if(fishList.get(i).x == x && fishList.get(i).y ==y) {
 				return fishList.get(i);
@@ -60,7 +60,7 @@ public class TennagerShark{
 		return null;
 	}
 	
-	public static Fish findById(int id) {
+	public static Fish findById(int id, List<Fish> fishList) {
 		for(int i=0; i<fishList.size(); i++) {
 			if(fishList.get(i).id == id) {
 				return fishList.get(i);
@@ -73,8 +73,8 @@ public class TennagerShark{
 		
 		for(int i=0; i<4; i++) {
 			for(int j=0; j<4; j++) {
-				if(findByCoord(i,j).alive ==true) {
-					fishes[i][j] = findByCoord(i, j).id;
+				if(findByCoord(i,j, fishList).alive ==true) {
+					fishes[i][j] = findByCoord(i, j, fishList).id;
 				}else {
 					fishes[i][j] = -1;
 				}
@@ -90,9 +90,19 @@ public class TennagerShark{
 		
 	}
 	
+	public static List<Fish> deepCopyArrayList(List<Fish> arrayList) {
+		
+		List<Fish> copyList = new ArrayList<Fish>();
+		for(Fish fish: arrayList) {
+			copyList.add(fish);
+		}
+		return copyList;
+	}
 
 	
 	public static void main(String[] args) throws IOException {
+		
+		List<Fish> fishList = new ArrayList<>();
 		
 		//input
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -121,19 +131,19 @@ public class TennagerShark{
 //		System.out.println(fishList);
 		
 		//solution
-		solution();
+		solution(fishList);
 		
 		
 	}
 	
 	
-	public static void solution() {
+	public static void solution(List<Fish> fishList) {
 		
 		
 		//상어가 먹는다
 		
 		//초기 조건 (0,0)
-		Fish firstFish= findByCoord(0,0);
+		Fish firstFish= findByCoord(0,0,fishList);
 		Shark shark = new Shark(firstFish.dir, firstFish.id, 0, 0);
 		firstFish.alive = false;
 		System.out.println(shark);
@@ -143,23 +153,64 @@ public class TennagerShark{
 		for(int i=0; i<fishList.size(); i++) {
 			if(fishList.get(i).alive == true) {
 				//System.out.println(i+1 + "번째 입니다");
-				moveFish(fishList.get(i), shark);
+				moveFish(fishList.get(i), shark, fishList);
 				//print2DArray(fishList);
 				//System.out.println();
 			}
 		}
 		
 		//System.out.println(fishList);
+		List<Fish> copiedFish = deepCopyArrayList(fishList);
+		Shark copiedShark = new Shark(0, 0, 0, 0);
+		copiedShark = shark;
 		
 		
 		//상어가 이동한다.
-		
+		dfs(copiedFish, copiedShark);
+
 		
 		//상어가 먹는다
 		
 	}
 	
-	public static void moveFish(Fish fish, Shark shark) {
+	public static void dfs(List<Fish> originFishList, Shark shark) {
+		
+		int nx, ny;
+		for(int dist=0; dist<4; dist++) {
+			
+			
+			nx = shark.x + dx[shark.dir]*dist;
+			ny = shark.y + dx[shark.dir]*dist;
+			
+			//상어가 먹는다
+			
+			Fish fish= findByCoord(nx,ny,originFishList);
+			Shark newShark = new Shark(fish.dir, fish.id, nx, ny);
+			fish.alive = false;
+			
+			print2DArray(originFishList);
+			System.out.println();
+			
+			//물고기가 이동한다.
+			for(int i=0; i<originFishList.size(); i++) {
+				if(originFishList.get(i).alive == true) {
+					//System.out.println(i+1 + "번째 입니다");
+					moveFish(originFishList.get(i), shark, originFishList);
+					//print2DArray(fishList);
+					//System.out.println();
+				}
+			}
+			
+			//복제한다
+			List<Fish> copiedFish = deepCopyArrayList(originFishList);
+			
+
+					
+		}
+
+	}
+	
+	public static void moveFish(Fish fish, Shark shark, List<Fish> fishList) {
 		
 		int nx, ny;
 		int newDir;
@@ -180,7 +231,7 @@ public class TennagerShark{
 				//이동할수 있는 조건
 				int tempX, tempY, tempDir;
 				fish.dir = newDir;
-				Fish targetfish = findByCoord(nx, ny);
+				Fish targetfish = findByCoord(nx, ny, fishList);
 				//교환 -> 내가 실수한 부분 -> 새로운 방향으로 집어 넣어야 하는데 
 				tempX = fish.x; tempY = fish.y; 
 				fish.x = targetfish.x; fish.y = targetfish.y;
