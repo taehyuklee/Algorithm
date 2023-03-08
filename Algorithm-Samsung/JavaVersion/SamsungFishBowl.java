95e9d71b6ef992edc6c4d2b66d18f203c49aa935
TayLee <89365465+taehyuklee@users.noreply.github.com>
2023-03-08 21:17:43 +0900

    commit) solved
    백준 23291번 어항 정리 풀이

--- Algorithm-Samsung/JavaVersion/SamsungFishBowl.java
+++ Algorithm-Samsung/JavaVersion/SamsungFishBowl.java
@@ -8,7 +8,7 @@ public class Main {
 	static int[][] board, board2;
 	static int[] straight, straight2;
 	static int[] rangeArr = new int[4];
-	static int stInx_c, endInx_c, stInx_r, endInx_r;
+	static int stInx_c, endInx_c, stInx_r, endInx_r, answer;
 	
 	public static void print2D(int[][] array) {
 		for(int i=0; i<array.length; i++) {
@@ -33,6 +33,12 @@ public class Main {
 		}
 	}
 	
+	public static void copyArray(int[] source, int[] target) {
+		for(int i=0; i<source.length; i++) {
+			target[i] = source[i];
+		}
+	}
+	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
@@ -52,40 +58,54 @@ public class Main {
 			straight2[j] = straight[j];
 		}
 		
-		//print2D(board);
-		
-		//print1D(straight);
-		
+
 		solution();	
 		
 	}
 	
 	public static void solution() {
+		answer=0;
 		
-		//1. 물고기가 가장 적은 곳에 한마리씩 넣어주기
-		putOne();
-		
-		//print1D(straight2);
-		
-		//2. board에 삽입하기
-		insertBoard();
-		
-		//print2D(board);
-		
-		
-		//3. 어항을 쌓는다 90도
-		stack90();
-		
-		//4. 물고기 수 조절
-		controlFish();
+		while(true) {
+			//1. 물고기가 가장 적은 곳에 한마리씩 넣어주기
+			putOne();
+			
+			//2. board에 삽입하기
+			insertBoard();
+	
+			//3. 어항을 쌓는다 90도
+			stack90();
+			
+			//4. 물고기 수 조절
+			controlFish();
+	
+			//5. 일렬화 하기.
+			straighten(board);
+	
+			//6. 반 접어서 180도 공중부양. (두번 접기)
+			int[][] halfed2D= half180();
+			
+			//7. board에 insert
+			insert2Dboard(halfed2D);
+	
+			//8. 물고기 control
+			controlFish();
+	
+			//9. 일렬화
+			straighten(board);
+			
+			//10. 최대와 최소 차이
+			copyArray(straight, straight2);
+			
+			answer+=1;
+			Arrays.sort(straight2);
+			
+			if(straight2[straight2.length-1] - straight2[0] <=K) {
+				break;
+			}
+		}
 
-		//5. 일렬화 하기.
-		straighten(board);
-
-		//6. 반 접어서 180도 공중부양.
-		half180();
-		
-
+		System.out.println(answer);
 		
 	}
 	
@@ -114,6 +134,18 @@ public class Main {
 		for(int j=0; j<N; j++) {
 			board[N-1][j] = straight[j];
 		}
+
+	}
+	
+	public static void insert2Dboard(int[][] halfed2D) {
+		//물고기 control format에 맞춰줘야 한다.
+		board = new int[N][N];
+		board2 = new int[N][N];
+		for(int i=halfed2D.length-1; i>=0; i--) {
+			for(int j=0; j<halfed2D[0].length; j++) {
+				board[i][j] = halfed2D[i][j];
+			}
+		}
 	}
 	
 	public static void stack90() {
@@ -123,7 +155,6 @@ public class Main {
 		
 		while(true) {
 			//공중부양할 range를 구한 {return column (start, end) row (start, end)}
-		//for(int i=0; i<2; i++) {
 			get2DRange();
 			
 			//넘어가면 바로 stop
@@ -133,13 +164,8 @@ public class Main {
 			
 			//공중 부양시켜서 90도 회전한다음 board위에 stack을 쌓는다.
 			rotate90Stack();
-//			print2D(board);
-//			System.out.println();
-			
-			//}
 		}
 		
-		print2D(board);
 
 	}
 	
@@ -148,7 +174,6 @@ public class Main {
 		int inter = board[N-1][0];
 		board[N-1][0] = 0;
 		board[N-2][1] = inter;
-		
 	}
 	
 	public static void get2DRange() {
@@ -170,19 +195,15 @@ public class Main {
 		}
 		
 		for(int j=0; j<N; j++) {
-			
 			//윗 블록은 어디까지 존재하는지? (어디까지 공중부양시켜야 하나?)
 			if(board[N-2][j] !=0) {
 				endInx_c=j;
 			}
 		}
-		
-		
+
 		//위에 쌓여 있는지에 대한 여부
 		if(endInx_c != -1) {
 			//위에 쌓여있다.
-			
-			//어디까지 쌓여 있는지?
 			for(int i=N-1; i>=0; i--) {
 				if(board[i][endInx_c] ==0) {
 					endInx_r = i+1;
@@ -192,22 +213,11 @@ public class Main {
 			
 		}
 		
-		//array에 정리해서 넣어주기
-		//int[] rangeArr = new int[4];
-
-//		rangeArr[0] = stInx_c;
-//		rangeArr[1] = endInx_c;
-//		rangeArr[2] = stInx_r; //맨 아
-//		rangeArr[3] = endInx_r; // 위로 
-//		
-//		
-//		return rangeArr;
-		
 	}
 	
 	public static void rotate90Stack() {
 		
-		//부양하는 사각형에 맞춰 새로운 array 생
+		//위로 부양하는 사각형에 맞춰 새로운 array 생성
 		int newC = endInx_c-stInx_c+1;
 		int newR = stInx_r-endInx_r+1;
 		int[][] goUp = new int[newR][newC];
@@ -222,9 +232,7 @@ public class Main {
 				//System.out.println(board[i+(N-newR)][stInx_c+j]);
 			}
 		}
-		
-		//print2D(goUp);
-		
+
 		//여기서 90도 돌린다. 앞서 부양시킴.
 		int[][] rotate90 = new int[newC][newR];
 		
@@ -234,11 +242,7 @@ public class Main {
 				
 			}
 		}
-		
-		//System.out.println();
-		//print2D(rotate90);
-		//print2D(board);
-		
+
 		//그 다음 위에 stack을 쌓는다 
 		for(int i=0; i<rotate90.length; i++) {
 			for(int j=0; j<rotate90[0].length; j++) {
@@ -246,9 +250,6 @@ public class Main {
 				//goUp.length 이만큼 차이 이후에 쌓아야 한다.
 			}
 		}
-		
-		//print2D(board);		
-		
 	}
 
 	public static void controlFish() {
@@ -277,8 +278,6 @@ public class Main {
 								int absDiff = Math.abs(board[new_r][new_c] - board[i][j]);
 								int share = (int) absDiff/5;
 								
-								//System.out.println(share);
-								
 								if(share >0) {
 									
 									//많은 곳에서 적은곳으로 d만큼 이동 board2에 적
@@ -288,13 +287,9 @@ public class Main {
 										board2[new_r][new_c] += share;
 										
 									}else if(board[i][j] < board[new_r][new_c]){
-									
 										board2[i][j] += share;
-										board2[new_r][new_c] -= share;
-										
+										board2[new_r][new_c] -= share;	
 									}
-									
-									
 								}else if(share <0) {
 									continue;
 								}
@@ -306,7 +301,6 @@ public class Main {
 					visit[i][j] = true;
 					
 				}
-				
 			}
 		}
 		
@@ -318,23 +312,19 @@ public class Main {
 		
 		int order =0;
 		
-		for(int j=0; j<N; j++) {
-			for(int i=N-1; i>=0; i--) {
+		for(int j=0; j<array[0].length; j++) {
+			for(int i=array.length-1; i>=0; i--) {
 				
-				
-				if(board[i][j] != 0) {
-					
+				if(array[i][j] != 0) {
 					straight[order] = array[i][j];
-					
 					order+=1;
 					
-				}
-				
+				}	
 			}
 		}
 	}
 	
-	public static void half180() {
+	public static int[][] half180() {
 		
 		//board 초기화 
 		board = new int[N][N];
@@ -360,29 +350,23 @@ public class Main {
 		int[][] rotate2D = new int[1*4][(int) N/4];
 		
 		int halAndHalf = (int) rotatedBoard[0].length/2;
-		print2D(rotatedBoard);
-		
 
 		for(int i=0; i< 2; i++) {
 			for(int j=halAndHalf; j<rotatedBoard[0].length; j++) {
 				//180도 회전
-				System.out.println(rotate2D.length-1 -i);
-				rotate2D[j][rotate2D.length-1 -i] = rotatedBoard[rotatedBoard.length-1 -i][j];
+				//System.out.println(rotate2D.length-1 -i);
+				rotate2D[rotate2D.length-1 -i][j-halAndHalf] = rotatedBoard[rotatedBoard.length-1 -i][j];
 			}
 		}
-		print2D(rotate2D);
-		
+
 		for(int i=0; i< 2; i++) {
 			for(int j=0; j<halAndHalf; j++) {
 				//180도 회전
-				System.out.println(N/4-1-j + " ////");
-				rotate2D[N-3-i][N/4-1-j] = rotatedBoard[i][j];
+				rotate2D[(rotate2D.length-1)-2-i][N/4-1-j] = rotatedBoard[i][j];
 			}
 		}
-		
-		print2D(rotatedBoard);
-		print2D(rotate2D);
-		
+
+		return rotate2D;
 	}
 
 	public static int[][] get2D() {
@@ -399,11 +383,8 @@ public class Main {
 				goUp[i][j] = board[i+(N-newR)][stInx_c+j];
 				//공중부양 시킨 부분은 모두 0으로
 				board[i+(N-newR)][stInx_c+j]=0;
-				//System.out.println(board[i+(N-newR)][stInx_c+j]);
 			}
 		}
-		
 		return goUp;
-
 	}
 }
