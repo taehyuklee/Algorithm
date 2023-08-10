4182270957f1ffb633c8e0bb77df6bb4ccad7ff5
TayLee <89365465+taehyuklee@users.noreply.github.com>
2023-08-11 01:06:41 +0900

    Create BAEKJOON #2164 - card2.java

---
+++ BAEKJOON/BAEKJOON #2164 - card2.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+	
+	static int N;
+	static List<Integer> deque = new ArrayList<>();
+	static Queue<Integer> Q = new LinkedList<>();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		for(int i=1; i<=N; i++) {
+			Q.add(i); 
+		}
+		
+		while(Q.size() !=1) {
+			throwAway();
+			topToDown();
+		}
+		
+		System.out.println(Q.peek());
+	}
+	
+	static void throwAway() {
+		Q.poll();
+	}
+	
+	static void topToDown() {
+		int tempInt = Q.poll();
+		Q.add(tempInt);
+	}
+	
+}
