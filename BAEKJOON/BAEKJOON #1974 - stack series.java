62ec6199a3fc66fc632cf3ef90c233560d43f7b6
TayLee <89365465+taehyuklee@users.noreply.github.com>
2023-08-13 01:49:50 +0900

    Create BAEKJOON #1974 - stack series.java

---
+++ BAEKJOON/BAEKJOON #1974 - stack series.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	static int N;
+	static Stack<Integer> stack = new Stack<>();
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		N = sc.nextInt();
+		
+		int start = 0;
+		
+		for(int i=0; i<N; i++) {
+			
+			int value = sc.nextInt();
+			
+			if(start<value) {
+				for(int j=start+1; j<=value; j++) {
+					stack.push(j);
+					sb.append("+").append("\n");
+				}
+				start= value;
+			}
+			
+			if(stack.peek() == value) {
+				stack.pop();
+				sb.append("-").append("\n");
+			}else {
+				System.out.println("NO");
+				return;
+			}
+		}
+		
+		sb.deleteCharAt(sb.length()-1);
+		System.out.println(sb);
+		
+	}
+	
+	static void solution() {
+		
+	}
+}
