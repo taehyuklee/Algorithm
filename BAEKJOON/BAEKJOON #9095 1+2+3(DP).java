adcc10a0e5f980316ee4684614697c45d48ba148
TayLee <89365465+taehyuklee@users.noreply.github.com>
2023-08-17 11:20:08 +0900

    Create BAEKJOON #9095 1+2+3(DP).java

---
+++ BAEKJOON/BAEKJOON #9095 1+2+3(DP).java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+	
+	static int N;
+	static int[] dp;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		
+		for(int i=0; i<N; i++) {
+			int num = sc.nextInt();
+			
+			dp = new int[11];
+			dp[1] = 1;
+			dp[2] = 2;
+			dp[3] = 4;
+			
+			for(int n=4; n<=num; n++) {
+				dp[n] = dp[n-1] + dp[n-2] + dp[n-3];
+			}
+			System.out.println(dp[num]);
+		}
+		
+	}
+}
