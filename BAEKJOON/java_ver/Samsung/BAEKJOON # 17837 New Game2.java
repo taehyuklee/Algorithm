04bd265becd110c2df716448c2880eadfb7367d6
TayLee <89365465+taehyuklee@users.noreply.github.com>
2023-09-21 01:19:15 +0900

    Update BAEKJOON # 17837 New Game2.java

--- BAEKJOON/java_ver/Samsung/BAEKJOON # 17837 New Game2.java
+++ BAEKJOON/java_ver/Samsung/BAEKJOON # 17837 New Game2.java
@@ -14,7 +14,8 @@ class Horse{
 public class Main {
 	
 	static int N, K;
-	static int[][] board;
+	static int[][] colorBoard;
+	static List<Horse>[][] horseBoard;
 	static int[] dx = {0,0,-1,1}, dy = {1,-1,0,0};
 	static List<Horse> horseList= new ArrayList<Horse>();
 
@@ -26,11 +27,12 @@ public class Main {
 		N = sc.nextInt();
 		K = sc.nextInt();
 		
-		board = new int[N][N];
+		colorBoard = new int[N][N];
+		horseBoard = new ArrayList[N][N];
 		
 		for(int i=0; i<N ; i++) {
 			for(int j=0; j<N; j++) {
-				board[i][j] = sc.nextInt();
+				colorBoard[i][j] = sc.nextInt();
 			}
 		}
 		
@@ -39,6 +41,7 @@ public class Main {
 			int y = sc.nextInt()-1;
 			int dir = sc.nextInt()-1;
 			Horse horse = new Horse(x,y,dir);
+			horseBoard[x][y].add(horse);
 			horseList.add(horse);
 		}
 		
@@ -48,16 +51,48 @@ public class Main {
 	
 	static void solution(){
 		
-		for(int i=0 ;i<K; i++) {
-			Horse horse = horseList.get(i);
+		//말이 한 바퀴 돌아감
+
+			for(int i=0 ;i<K; i++) {
+				Horse horse = horseList.get(i);
+				
+				//미리 탐험
+				int new_x = horse.x +dx[horse.dir];
+				int new_y = horse.y +dy[horse.dir];
+				
+				//움직였을때 빨강이냐, 파랑이냐, 흰색이냐, 아님 벗어난거냐?
+				if(colorBoard[new_x][new_y] == 2 || new_x<0 || new_y<0 || new_x>N || new_y>N) { //파랑
+					reverseDir(horse);
+					move(horse);
+					continue;
+					
+				}else if(colorBoard[new_x][new_y] ==1) { //빨강
+
+					
+				}else {
 			
-			int new_x = horse.x +dx[horse.dir];
-			int new_y = horse.y +dy[horse.dir];
-			
-			
-			
+					
+				}
+				
+				
+				horse.x = new_x; // 저장
+				horse.y = new_y; // 저장
+				
 		}
+			
 		
+	}
+
+	static void move(Horse horse) {
+		horseBoard[horse.x][horse.y].remove(horse); //기존 horse판에서 지워주고
+		horse.x = horse.x +dx[horse.dir];
+		horse.y = horse.y +dy[horse.dir];
+		horseBoard[horse.x][horse.y].add(horse); //판에 넣어준다.
+		
+	}
+	
+	static void reverseDir(Horse horse) {
+		horse.dir = (horse.dir +2)%4;
 	}
 	
 }
