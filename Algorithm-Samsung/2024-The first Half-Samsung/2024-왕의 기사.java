2268235d676c750d892c318c68ace2289d436d58
TayLee <89365465+taehyuklee@users.noreply.github.com>
2024-04-10 01:48:22 +0900

    Update 2024-왕의 기사.java

--- Algorithm-Samsung/2024-The first Half-Samsung/2024-왕의 기사.java
+++ Algorithm-Samsung/2024-The first Half-Samsung/2024-왕의 기사.java
@@ -20,13 +20,182 @@ import java.util.*;
  * Q번 명령이 주어짐 
  * Q번 이후 생존한 기사들이 총 받은 대미지의 합을 출력 
  * 
- * 
+ *  주어진 격자에서 0 - 빈칸, 1- 함정, 2 - 벽 을 의미한다
+ *  N개의 줄에 걸쳐 초기 기사들의 정보가 주어진다 (r, c, h, w, k) 
+ *  (입력은 1번 기사부터 N번 기사까지 순서대로 정보가 주어진다)
+ *  초기 기사들의 위치는 기사끼리 또는 벽과 겹치지 않는다.
+ *  
+ *  다음 Q개의 줄에 걸쳐 왕의 명령이 주어진다 (i, d)  i 번 기사에게 방향 d로 한 칸 움직이라 
+ *  1<= i <= N (이미 사라진 기사 번호가 주어질 수도 있다) 
+ *  d = {0,1,2,3} 위 오 아 왼 
  * */
 
-public class Main {
-
-	public static void main(String[] args) {
-		
+class Kight{
+	int id, r, c, h, w, k;
+	boolean out;
+	
+	public Kight(int id, int r, int c, int h, int w, int k) {
+		this.id = id;
+		this.r = r;
+		this.c = c;
+		this.h = h;
+		this.w = w;
+		this.k = k;
+		this.out = false;
+	}
+	
+	@Override
+	public String toString() {
+		return "id: " + id + ", r: " + r + ", c: " + c + ", h: " + h + ", w : " + w + ", k: " + k + " + " + ", out : " + out + " ||";
 	}
 	
 }
+
+public class Main {
+	
+	static int L, N, Q;
+	static int[][] envMap;
+	static int[] dx = {-1,0,1,0}, dy = {0,1,0,-1};
+	static int[][] kightMap, kightMap2;
+	static List<Kight> listKight;
+	static List<int[]> orderList;
+	
+	public static void print1D(List<int[]> array) {
+		for(int i=0; i<array.size(); i++) {
+			System.out.println(array.get(i)[0] + " , " +array.get(i)[1]);
+		}
+	}
+
+	public static void print2D(int[][] map) {
+		for(int i=0; i<kightMap.length; i++) {
+			for(int j=0; j<kightMap[0].length; j++) {
+				System.out.print(map[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		L = sc.nextInt(); //격자 크기
+		N = sc.nextInt(); //기사의 숫자
+		Q = sc.nextInt(); //왕의 명령 횟수
+		
+		envMap = new int[L][L];
+		kightMap = new int[L][L];
+		kightMap2 = new int[L][L];
+		listKight = new ArrayList<>();
+		orderList = new ArrayList<>();
+		
+		for(int i=0; i<L; i++) {
+			for(int j=0; j<L; j++) {
+				envMap[i][j] = sc.nextInt();
+			}
+		}
+		
+		for(int i=1; i<=N; i++) {
+			int r = sc.nextInt()-1; 
+			int c = sc.nextInt()-1; 
+			int h = sc.nextInt(); 
+			int w = sc.nextInt();
+			int k = sc.nextInt();
+			Kight kight = new Kight(i,r,c,h,w,k);
+			listKight.add(kight);
+		}
+		
+		for(int q=0; q<Q; q++) {
+			int i = sc.nextInt(); 
+			int d = sc.nextInt(); 
+			int[] order = new int[] {i,d};
+			orderList.add(order);
+		}
+		
+		drawKightMap();
+		//print2D(kightMap);
+
+		solution();
+		
+	}
+	
+	public static void solution() {
+		
+		//order
+		for(int turn=0; turn<1; turn++) {
+			
+			int[] order = orderList.get(turn);
+			int i = order[0];
+			int d = order[1];
+			
+			//Phase1. 왕의 명령			
+			//기사 선택
+			Kight pickedKight = findKightById(i);
+			if(pickedKight.out == true) continue; //pickedKight가 out이라면 다음 order로 넘긴다.
+			
+			
+			//기사 이동
+			moveKight(i, d);
+			
+			
+			//대결 대미지
+			
+			
+			
+			
+		}
+
+	}
+	
+	static Kight findKightById(int id) {
+		for(int i=0; i<listKight.size(); i++) {
+			if(listKight.get(i).id == id) {
+				return listKight.get(i);
+			}
+		}
+		return null;
+	}
+	
+	static void drawKightMap() {
+		for(int i=0; i<listKight.size(); i++) {
+			Kight kight = listKight.get(i);
+			
+			for(int j=0; j<kight.h; j++) {
+				for(int k=0; k<kight.w; k++) {
+					kightMap[kight.r+j][kight.c+k] = kight.id;
+				}
+			}
+			
+		}
+	}
+	
+	static void moveKight(int id, int dir) {
+		
+		List<int[]> listed = moving(id, dir);
+		
+		//움직였을때 그 위치에 혹시나 그 기사가 아닌 다른 기사가 있으면 모두 밀린다. 
+
+		
+		
+	}
+	
+	static List<int[]> moving(int id, int dir) {
+		List<int[]> listInt = new ArrayList<>();
+		for(int i=0; i<L; i++) {
+			for(int j=0; j<L; j++) {
+				if(i ==kightMap[i][j]) {
+					int newI = i+dx[dir];
+					int newJ = j+dy[dir];
+					kightMap2[newI][newJ] = kightMap[i][j];
+					listInt.add(new int[] {newI, newJ}) ;
+				}
+			}
+		}
+		return listInt;
+	}
+	
+	
+}
+
+
+
