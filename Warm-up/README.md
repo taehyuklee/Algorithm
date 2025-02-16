9280d9b2811c7b27fb560ed716445ad4f43c32e2
TayLee <thlee991@gmail.com>
2025-02-16 16:03:28 +0900

    commit)

---
+++ Warm-up/README.md
@@ -0,0 +1,109 @@
+# Python Coding Test Warming-up
+<p>2022년부터 2024년까지는 Java로 코딩 테스트를 준비했다. 물론 개발에서는 Java, Python 둘다 사용해왔다. 2025년에는 Python으로 다시 코딩 테스트를 준비하고자 한다. 코딩테스트로 Java를 사용할 때 느꼈던 가장 큰 이유 중 하나는 타이핑해야 할 코드가 너무 많았다는 점이다. (물론 Java는 타입을 명시할 수 있어 명확하다는 장점이 있다)
+: 현 시점 2025-02-16</p>
+
+## Python Grammar (자료구조 관련 필수 문법)
+<h3> <b>[ Input ]</b> </h3>
+Python에서 Input정보를 가져오기 위해 input() 내장함수를 사용하거나 sys.readline()을 사용해야 한다.
+
+<br>
+
+
+
+<h3> <b>[ Python List append (Push) & insert ]</b> </h3>
+Python의 List에 어떤 값을 마지막 index 다음에 추가할때 사용 
+
+### 1. append
+우리가 일반적으로 Stack구조에서 사용하는 Push랑 같다 마지막 index다음에 특정 데이터를 삽입하게 된다.
+
+``` python
+# List자료구조는 push할때 마지막에 push된다. : 복잡도 O(N)
+python_list = [1,2,3,4,5,6,7]
+python_list.append(100)
+
+print(python_list)
+
+# Result
+[1,2,3,4,5,6,7,100]
+```
+<br>
+
+### 2. insert
+ 특정 위치에 Insert를 하고 싶을때, insert method를 사용한다. 구조는 ```list.insert(index, value)``` 형태로 되어 있으며, 원하는 value를 원하는 index에 넣을 수 있다. <br>
+해당 방법은 특정 index까지 찾는 것은 O(1)이 될 수 있으나, 뒤쪽 요소들을 이동해야 하므로 최악의 경우 O(N)이 발생한다.
+
+``` python
+# List자료구조는 insert는 뒤의 요소들 이동. : 복잡도 O(N)
+python_list = [1,2,3,4,5,6,7]
+python_list.insert(3, 100)
+
+print(python_list)
+
+# Result
+[1,2,3,100,4,5,6,7]
+```
+<br><br>
+
+
+<h3> Python List pop & remove (pop)</h3>
+
+Python의 List에서 어떠한 값을 객체를 제거할때 사용한다. index로 접근해서 제거하는 것과, 객체 내용을 보고 제거하는 것 두 가지 방법이 있다. 각각 pop, remove method에 해당된다.
+
+### 1. pop
+Python의 List에서 특정 위치의 값을 제거하고 싶을때 pop을 이용한다. (<b>index로 접근하여 해당 index 값을 빼낸다. 반환값은 해당 데이터가 반환 된다.</b>) ``` list.pop(index) ``` 형태를 가지고 있다.
+<b>index 접근, 맨 처음, 마지막 O(1), 중간 index O(N) 특징을 갖는다.</b>
+
+``` python
+#
+python_list = [1,2,3,4,5,6,7,999]
+pop_data1 = python_list.pop() # 마지막 index가 pop됨. 
+pop_data2 = python_list.pop(3) # 3번째 index내용이 pop됨.
+
+
+# Result (LIFO)
+999 # pop_data1
+4 # pop_data2
+
+```
+<br>
+
+
+### 2. remove
+특정 내용을 기준으로 제거하게 된다. 아래의 예시에서 999를 제거하고 싶어 그러면 ```list.remove(999)```를 하게 되면 999가 제거된다.
+
+``` python
+# List자료구조에서 즉정 내용을 확인해야하므로 : 복잡도 (O(N))
+python_list = [1,2,3,999,4,5,6,7,]
+pop_data = python_list.remove(999) 
+
+print(pop_data)
+
+# Result (LIFO)
+999
+```
+<br><br>
+
+
+
+<h3> Python Map (Dictionary) </h3>
+'''
+Python에서는 Map구조를 Dictionary라는 자료구조로 명명해서 사용하고 있다. 
+'''
+
+```python
+python_map = {}
+
+python_map["1"] = "1"
+
+print(python_map)
+print(type(python_map))
+```
+<br><br>
+
+
+<h3> Python Queue (Dequeue) </h3>
+
+<br>
+
+
+<h3> Python Output </h3>
--- Warm-up/python_warmup_20250216.py
+++ Warm-up/python_warmup_20250216.py
@@ -1,58 +1,109 @@
 # Python 문법 다시 한 번 각인 시키기
-from typing import List, Any
+# from typing import List, Any
 
-arr : List[Any] = [(1, 5), (2, 3), (1, 2), (3, 1)]
+# arr : List[Any] = [(1, 5), (2, 3), (1, 2), (3, 1)]
 
-# 두 번째 요소(인덱스 1)를 기준으로 정렬
-sorted_arr = sorted(arr, key=lambda x: x[1])
-# print(sorted_arr)
+# # 두 번째 요소(인덱스 1)를 기준으로 정렬
+# sorted_arr = sorted(arr, key=lambda x: x[1])
+# # print(sorted_arr)
 
-job = lambda x: x[1]
+# job = lambda x: x[1]
 
-# print(job(arr))
+# # print(job(arr))
 
 
-# Python에서의 sort와 sorted 차이점
+# # Python에서의 sort와 sorted 차이점
 
-arr = [1,3,1,23,7,4,2,67,78]
+# arr = [1,3,1,23,7,4,2,67,78]
 
-# 원본을 정렬시켜버림 Instance (Object)내의 method
-arr.sort()
+# # 원본을 정렬시켜버림 Instance (Object)내의 method
+# arr.sort()
 
+# # print(arr)
+
+# # sorted() 원본이 아닌 새로운 list를 만들어서 반환함 
+# sortedArr = sorted(arr) # Python의 Global 내장 함수
+
+# # print(sortedArr)
+
+# # Key로 정렬
+# arr = [("orange", 5), ("banana", 2), ("apple", 3)]
+
+# def get_first_element(x):
+#     return x[0]  # 첫 번째 요소를 반환
+
+# arr.sort(key=get_first_element)
+
+# # print(arr)
+
+
+# arr = [("apple", 5), ("banana", 2), ("orange", 3)]
+
+# # key 함수 정의
+# def get_first_element(x):
+#     print(f"get_first_element({x}) = {x[0]}")  # 호출된 함수와 그 결과 출력
+#     return x[0]
+
+# # sort 호출 전에 원본 리스트 출력
+# print("Before sort:")
 # print(arr)
 
-# sorted() 원본이 아닌 새로운 list를 만들어서 반환함 
-sortedArr = sorted(arr) # Python의 Global 내장 함수
+# # sort 실행 (key는 get_first_element)
+# arr.sort(key=get_first_element)
 
-# print(sortedArr)
+# arr.sort(key=lambda x: x[1])
 
-# Key로 정렬
-arr = [("orange", 5), ("banana", 2), ("apple", 3)]
-
-def get_first_element(x):
-    return x[0]  # 첫 번째 요소를 반환
-
-arr.sort(key=get_first_element)
-
+# # sort 후 결과 출력
+# print("\nAfter sort:")
 # print(arr)
 
 
-arr = [("apple", 5), ("banana", 2), ("orange", 3)]
+############### Python Basic Grammar  #################
+# Input
 
-# key 함수 정의
-def get_first_element(x):
-    print(f"get_first_element({x}) = {x[0]}")  # 호출된 함수와 그 결과 출력
-    return x[0]
 
-# sort 호출 전에 원본 리스트 출력
-print("Before sort:")
-print(arr)
+# List push, pop 
+python_list = [1,2,3,4,5,6,7]
 
-# sort 실행 (key는 get_first_element)
-arr.sort(key=get_first_element)
+# List에 Push
+python_list.append(999)
 
-arr.sort(key=lambda x: x[1])
+print(python_list)
 
-# sort 후 결과 출력
-print("\nAfter sort:")
-print(arr)
+# 어떠한 값을 객체를 제거할때 사용 
+
+# Python에서 특정 객체를 제거할때 사용. (객체의 내용을 보고 즉, 내용을 확인해야하므로 O(N))
+# removed_data = python_list.remove(3)
+
+# Python에서 특정 index에 있는 data를 pop한다. (index 접근, 맨 처음, 마지막 O(1), 중간 index O(N))
+pop_data = python_list.pop()
+print(pop_data)
+
+# 일반적으로index로 접근하게 되면 O(1) 걸리겠지만, 그냥 객체를 찾아서 제거하기 위해서는 Worst (O(N)) 이 걸린다. 
+
+# 특정 index에 특정 값을 넣기 위함. (O(N))
+python_list.insert(3, "hi")
+
+print(python_list)
+
+
+
+# Map push, remove
+'''
+Python에서는 Map구조를 Dictionary라는 자료구조로 명명해서 사용하고 있다. 
+'''
+python_map = {}
+
+python_map["1"] = "1"
+
+print(python_map)
+print(type(python_map))
+
+
+
+# Dequeue
+
+
+
+
+# Python 문자열 출력 방식 
