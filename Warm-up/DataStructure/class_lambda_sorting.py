05e2e1f7a9a2a07546e770e983edc7a0cd4539de
TayLee <thlee991@gmail.com>
2025-02-22 23:09:49 +0900

    commit)

--- Warm-up/DataStructure/class_lambda_sorting.py
+++ Warm-up/DataStructure/class_lambda_sorting.py
@@ -1,10 +1,13 @@
-class ListElement:
+class Element:
     def __init__(self, x, y, z):
         self.x: int = x
         self.y: str = y
         self.z: long = z
 
-    def __str__(self):
+    # def __str__(self):
+    #     return "x: " + str(self.x) + " y: " + str(self.y) + " z: " + str(self.z)
+    
+    def __repr__(self):
         return "x: " + str(self.x) + " y: " + str(self.y) + " z: " + str(self.z)
 
 x_list = [1,2,3,5,5,6,7,7,9,10]
@@ -16,8 +19,12 @@ z_list = [3,1,2,5,1,3,2,3,1,4]
 
 #     print(listElement)
 
+listElement = []
 for i in range(10):
-    listElement = ListElement(x_list[i], str_list[i], z_list[i])
-    print(listElement)
+    element: Element = Element(x_list[i], str_list[i], z_list[i])
+    listElement.append(element)
+    print(element)
 
+
+print(listElement)
 # sort에서 같은것이 있으면 그 다음 요소로 sort하는것들 
