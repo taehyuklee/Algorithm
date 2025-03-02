be47c2be96ac24543729f7edc519f5904520b298
TayLee <89365465+taehyuklee@users.noreply.github.com>
2025-03-02 15:07:21 +0900

    Update findMissingNum.py

--- Codility/findMissingNum.py
+++ Codility/findMissingNum.py
@@ -7,3 +7,22 @@ def solution(A):
     for i in range(1,len(A)+1):
         if i != A[i-1]:
             return i
+
+############ 
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(A):
+
+    if not A:
+        return 1
+
+    # Implement your solution here
+    A.sort()
+    for i in range(1,len(A)+1):
+        if i != A[i-1]:
+            return i
+    
+    return A[len(A)-1] + 1
+
+
