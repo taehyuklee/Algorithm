b69093f079bf45e9fbabb6894f1bd41570121577
TayLee <89365465+taehyuklee@users.noreply.github.com>
2025-03-02 19:07:01 +0900

    Update PairsOfCars.py

--- Codility/PairsOfCars.py
+++ Codility/PairsOfCars.py
@@ -73,3 +73,30 @@ def solution(A):
         return -1
         
     return sum_answer
+
+
+
+#### 시작이 1일때 ####
+def solution(A):
+    python_list = []
+    answer=0
+    sum_answer = 0
+
+    for i in range(len(A)):
+        if A[0]==0 and A[i] == 0:
+            python_list.append([])
+            python_list[len(python_list)-1].append(0)
+        elif A[0] ==1 and A[i] == 1:
+            python_list.append([])
+            python_list[len(python_list)-1].append(0)
+        else:
+            python_list[len(python_list)-1][0] += 1
+
+    for i in range(len(python_list)-1, -1, -1):
+        answer = python_list[i][0] + answer
+        sum_answer+=answer
+
+    if sum_answer > 1000000000:
+        return -1
+        
+    return sum_answer
