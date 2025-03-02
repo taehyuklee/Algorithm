e376df238b4f396c70acc8e9ab816d1d7f11e52a
TayLee <89365465+taehyuklee@users.noreply.github.com>
2025-03-02 11:46:35 +0900

    Update tow_pointer.py

--- BAEKJOON/python_ver/tow_pointer.py
+++ BAEKJOON/python_ver/tow_pointer.py
@@ -21,3 +21,33 @@ while left_point != N and right_point != N:
         left_point += 1
 
 print(answer_count)
+
+##################################################################
+#아래같이 풀어야함
+N, M = map(int, input().split())
+input_list = list(map(int, input().split()))
+
+left_point = 0
+right_point = 0
+answer_count = 0
+sub_sum = 0
+
+while left_point <= N and right_point <= N:
+    #right이 이미 벗어났다는것은 더 이상 전진의 의미가 없다 사실 ㄱ
+
+    if sub_sum < M:
+        if right_point < N:
+            sub_sum += input_list[right_point]
+        right_point += 1
+
+    elif sub_sum > M:
+        sub_sum -= input_list[left_point]
+        left_point +=1
+
+    else:
+        answer_count +=1
+        sub_sum -= input_list[left_point]
+        left_point += 1
+    # print(str(left_point) + "   " + str(right_point))
+    # print(sub_sum)
+print(answer_count)
