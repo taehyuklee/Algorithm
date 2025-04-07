aa76dd95f0d1aa260df1b80e1d29ffc6bbba0bb1
TayLee <89365465+taehyuklee@users.noreply.github.com>
2025-04-07 14:01:41 +0900

    Update Target_Number.py

--- Programmers/Python/Target_Number.py
+++ Programmers/Python/Target_Number.py
@@ -66,3 +66,37 @@ numbers = [4, 1, 2, 1]
 target = 4
 
 print(solution(numbers, target))
+
+
+################ 규칙 찾아서 좀 더 간단하게 일반화 한것 ########################
+def recursive(summation, numbers, target, answer, cur_depth, tot_depth):
+    
+    if cur_depth == tot_depth:
+        if summation == target:
+            answer[0] += 1
+        return
+
+    for sign in [1, -1]:
+    
+        next_one = sign * numbers.pop(0)
+        summation += next_one
+        cur_depth += 1
+
+        recursive(summation, numbers, target, answer, cur_depth, tot_depth)
+
+        # 원상 복귀
+        summation -= next_one
+        cur_depth -= 1
+        numbers.insert(0, next_one)  # 이거 복구 안했었다.
+
+
+def solution(numbers, target):
+    answer = [0]
+    summation = 0
+    cur_depth = 0
+    tot_depth = len(numbers)
+
+    recursive(summation, numbers, target, answer, cur_depth, tot_depth)
+
+    return answer[0]
+
