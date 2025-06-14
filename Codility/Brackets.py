80b2b485cf3545558289e80c702ce3d0e8367706
TayLee <89365465+taehyuklee@users.noreply.github.com>
2025-06-14 15:03:39 +0900

    Create Brackets.py

---
+++ Codility/Brackets.py
@@ -0,0 +1,22 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+
+def solution(S):
+    # Implement your solution here
+    python_list = []
+    left_bracket = ['(', '{', '[']
+    right_bracket = [')', '}', ']']
+    map_bracket = {'(': ')', '{': '}', '[':']'}
+
+    for s in S:
+        if s in left_bracket:
+            python_list.append(s)
+
+        elif s in right_bracket:
+            poped_str = python_list.pop()
+            if s == map_bracket[poped_str]:
+                continue
+            else:
+                return 0
+
+    return 1
