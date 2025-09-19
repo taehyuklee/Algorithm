aeae414f3d3c0e7123d093975a9f600184883c23
TayLee <89365465+taehyuklee@users.noreply.github.com>
2025-09-19 09:23:11 +0900

    Create Problem1.py

---
+++ CodingTestReproduction/N-Company/Problem1.py
@@ -0,0 +1,37 @@
+# Naver Cloud Coding Test
+'''
+Given First Case
+A = [3, 1, 5, 3, 3, 4, 2]
+A = [3, 1, 5, 3, 3]
+A = [1, 5, 3]
+
+맨 앞 2개, 맨 뒤 두 개, 맨 앞, 뒤 두개 이렇게 3가지 케이스가 있다.
+
+Anaser: 3
+
+returns the maximum number of moves that can be performed on
+All performed moves have the same result
+
+Other Test cases
+A = [4,1,4,3,3,2,5,2]
+Answer: 6
+
+A = [1,9,1,1,1,1,1,1,8,1]
+Answer: 1
+
+A = [1,9,8,9,5,1,2]
+Answer: 3
+
+A = [1,1,2,3,1,2,2,1,1,2]
+Answer: 4
+
+My Solution: BackTracking
+'''
+
+from typing import List
+
+
+def solution(A: List[str]):
+    answer = 0
+
+    return answer
