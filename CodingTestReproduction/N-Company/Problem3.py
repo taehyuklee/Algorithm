2c2a3a712f5f8f66430422f9f3a79a07a53fc2a1
TayLee <89365465+taehyuklee@users.noreply.github.com>
2025-09-19 09:23:47 +0900

    Create Problem3.py

---
+++ CodingTestReproduction/N-Company/Problem3.py
@@ -0,0 +1,12 @@
+'''
+HH:MM:SS 시각이 존재한다. 사이에 존재하는 모든 시각에 대한 갯수를 Return 해줘야 한다
+Start: 10:01:03  End: 10:05:59
+Start: 09:10:05  End: 10:11:30
+Start, End가 주어짐. 그 사이 모든 시각을 계산해서 출력하시오.
+
+My Solution: Implementation
+'''
+
+def solution(Start: str, End:str):
+    answer = 0
+    return answer
