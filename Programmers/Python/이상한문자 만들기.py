11779a507151dba21a6304d2f2cc79f05b666afa
TayLee <89365465+taehyuklee@users.noreply.github.com>
2025-09-20 23:49:36 +0900

    Create 이상한문자 만들기.py

---
+++ Programmers/Python/이상한문자 만들기.py
@@ -0,0 +1,17 @@
+def solution(s):
+    strings = s.split(" ")
+    
+    answer = ''
+    for string in strings:
+        new_string = ''
+        for idx, s in enumerate(string):
+            
+            if (idx)%2 ==0:
+                # 짝수일 경우
+                new_string +=  s.upper()
+            else:
+                # 홀수일 경우
+                new_string +=  s.lower()
+        
+        answer+= new_string + " "
+    return answer[:-1]
